a303aec0921aafcbf29561aff62ecdd2c67de42d
tomcatcher <tom.catcher@gmail.com>
2024-02-22 01:53:12 +0100

    Added update functionality to hdcs.sh.

--- hdcs.sh
+++ hdcs.sh
@@ -4,10 +4,19 @@
 
 # Check if the first argument is given. If not, show usage and list of all scripts in the online repository.
 if [ -z "$1" ]; then
-    echo "Usage: hdcs <script>"
+    echo "Usage: hdcs <script> | update"
     echo "Available scripts:"
     curl -k https://signitron.com/scripts/ | grep "a href" | awk '{ print $5 }' | sed 's/href="//g' | awk -F\> '{ print $2 }' | awk -F\< '{ print $1 }' | grep -v Name | grep -v Parent
     exit 1
+fi
+
+# If first parameter is "update", update the script itself
+if [ "$1" == "update" ]; then
+    echo "Updating hdcs..."
+    curl -k -s https://signitron.com/scripts/hdcs.sh > /opt/hdcs.hdcs.sh
+    chmod +x /opt/hdcs.hdcs.sh
+    echo "hdcs updated!"
+    exit 0
 fi
 
 # DEBGUG: Print the first argument
