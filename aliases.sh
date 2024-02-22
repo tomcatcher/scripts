e006a8c450a69fc80f774485620ca6d830a28ddb
tomcatcher <tom.catcher@gmail.com>
2024-02-22 16:28:36 +0100

    Experimenting.

--- aliases.sh
+++ aliases.sh
@@ -3,6 +3,7 @@
 # This file loads my aliases at the beginning of each session. It is to be placed into /etc/profile.d/aliases.sh
 
 # Aliases
+alias ll='ls -lah'
 
 # Write the aliases to a file in /etc/profile.d/
 echo "alias ll='ls -lah'" > /etc/profile.d/aliases.sh
