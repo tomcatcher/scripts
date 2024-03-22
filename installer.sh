9bd1b5e2d8fe00a8d647dbbe0df69434bc00300c
tomcatcher <tom.catcher@gmail.com>
2024-03-22 09:57:11 +0100

    Some update.

--- installer.sh
+++ installer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install sudo, curl, git, nano
-sudo apt-get update
-sudo apt-get install -y sudo curl git nano
+apt-get update
+apt-get install -y sudo curl git nano
 
 # Move to /opt
 cd /opt
